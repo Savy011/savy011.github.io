b49f65bc989269dd742158c5950b519e803b364a
Savy <savy.novachez011@proton.me>
2025-08-26 22:58:37 +0530

    configure and use body font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import tailwindcss from '@tailwindcss/vite'
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        name: 'Archivo Narrow',
+        provider: fontProviders.fontsource(),
+        cssVariable: '--font-archivo-narrow',
+      },
+    ],
   },
 })
