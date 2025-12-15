ff4023e5f25709fb8d2fde8a02eaa2ee48055753
Savy <savy.novachez011@proton.me>
2025-12-16 01:12:20 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://savy011.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
